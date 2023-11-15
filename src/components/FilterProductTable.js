4b4020574cd1ffaec49ea6c0658763fcc4f53edc
Ralph Woiwode <rawoiwode@gmail.com>
2023-11-15 09:43:16 -0600

    Breaking up into separate components

---
+++ src/components/FilterProductTable.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import ProductTable from "./ProductTable";
+
+const FilterProductTable = ({ products }) => {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  );
+};
+
+export default FilterProductTable;
